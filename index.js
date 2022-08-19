d24630314338229c9617c6f282c3797ba85893db
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-08-19 12:29:36 +0530

    class interface

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function greeter(person) {
 }
 var user = { firstName: "Rohit", lastName: "Kumar" };
 console.log(greeter(user));
+var Student = /** @class */ (function () {
+    function Student(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + lastName;
+    }
+    return Student;
+}());
+function greeter1(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user1 = new Student("Rohit", "Kumar");
+console.log(greeter1(user1));
--- index.ts
+++ index.ts
@@ -29,3 +29,28 @@ console.log(greeter(user));
 
 
 
+class Student {
+    fullName: string;
+    constructor(
+        public firstName: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter1(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user1 = new Student("Rohit", "Kumar");
+
+console.log(greeter1(user1));
+
+
+
